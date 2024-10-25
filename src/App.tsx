f0207d5eb3ea1f3ba8b7681c4963fc82ea0595e1
Didier Nsengiyumva <nsedidier@gmail.com>
2024-10-25 10:20:40 +0200

    latest updates now

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
 import React from "react";
-import PlasmicDashboard from "./components/plasmic/mayfair_clarity_iq/PlasmicDashboard";
-
+import PlasmicDashboard from "./components/plasmic/mayfair_clarity_iq/main";
 
 function Index() {
   return (
